5aa97297c583acfb3caa092b5273741846443e5b
arksnorman <arksnorman@gmail.com>
2017-09-12 03:50:16 +0300

    Added dynamic php date

--- login-index.php
+++ login-index.php
@@ -35,7 +35,7 @@
         </form>
     </div>
     <div class="footer">
-        <p> Copyright 2017. All Rights Reserved. Developed by SSA</p>
+        <p> Copyright <?=date('Y');?>. All Rights Reserved. Developed by SSA</p>
     </div>
 </body>
 </html>
